31f853cfccf8f5c46432db64d905a129dbf22af0
KhadegaAlaa <khadegaalaaeldeen84@gmail.com>
2025-01-31 22:01:29 +0200

    Add Blocked Country Controller

---
+++ Countries/Controllers/BlockedCountryController.cs
@@ -0,0 +1,34 @@
+﻿using Countries.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Countries.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlockedCountryController : Controller
+    {
+        private readonly BlockCountryService _blockService;
+        public BlockedCountryController(BlockCountryService blockService)
+        {
+            _blockService = blockService;
+        }
+
+        [HttpPost("block")]
+        public IActionResult BlockCountry([FromBody] string countryCode)
+        {
+            if (_blockService.BlockCountry(countryCode)) return Ok("Country blocked.");
+            return Conflict("Country already blocked.");
+        }
+
+        [HttpDelete("block/{countryCode}")]
+        public IActionResult UnblockCountry(string countryCode)
+        {
+            if (_blockService.UnblockCountry(countryCode)) return Ok("Country unblocked.");
+            return NotFound("Country not found in block list.");
+        }
+
+        [HttpGet("blocked")]
+        public IActionResult GetBlockedCountries() => Ok(_blockService.GetBlockedCountries());
+
+    }
+}
